24f8f404dcba9b7fd79333e2e12cfbeadda9992c
usm2377s-ai <usm2377s@maidenerleghschools.co.uk>
2026-08-08 03:03:32 +0100

    Create NoHotbarAnimMod.java

---
+++ src/main/java/com/example/nohotbaranim/NoHotbarAnimMod.java
@@ -0,0 +1,10 @@
+package com.example.nohotbaranim;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoHotbarAnimMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// Nothing to do on startup - the mixins handle everything.
+	}
+}
